d7874cdfe7abcf68168385119924d398612c586d
premananda <narayanreddy355@gmail.com>
2026-05-24 14:58:07 +0530

    Changed Base Url

--- src/components/BaseUrl.jsx
+++ src/components/BaseUrl.jsx
@@ -2,7 +2,11 @@
 const token = localStorage.getItem("token")
 
 
+// export const BASE_URL = {
+//     url:"http://localhost:8080/api/v1",
+//     token
+// }
 export const BASE_URL = {
-    url:"http://localhost:8080/api/v1",
+    url:"https://servica-backend.onrender.com/api/v1",
     token
 }
